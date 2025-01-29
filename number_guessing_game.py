e8c5230a6dd31892aec04b49637e23c9556bc5a6
Abhishek Kumar Singh <abhis42124@gmail.com>
2025-01-29 22:08:26 +0530

    Update the code

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -26,20 +26,20 @@ import random
 # except ValueError:
 #     print("Please enter a valid number")
 
-import random 
+import random
 
 number_to_guess = random.randint(1, 100)
 
 while True:
-  try:
-    guess = int(input('Guess the number between 1 and 100: '))
+    try:
+        guess = int(input("Guess the number between 1 and 100: "))
 
-    if guess < number_to_guess:
-      print('Too low!')
-    elif guess > number_to_guess:
-      print('Too high!')
-    else:
-      print('Congratulations! You guessed the number.')
-      break
-  except ValueError:
-    print('Please enter a valid number')
+        if guess < number_to_guess:
+            print("Too low!")
+        elif guess > number_to_guess:
+            print("Too high!")
+        else:
+            print("Congratulations! You guessed the number.")
+            break
+    except ValueError:
+        print("Please enter a valid number")
